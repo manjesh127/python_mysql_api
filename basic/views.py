f61073f5455f0957376e379ba6775ffb5d5e5dde
manjesh singh <majeshsingh127@gmail.com>
2019-12-16 17:04:35 +0530

    update

--- basic/views.py
+++ basic/views.py
@@ -3,7 +3,11 @@ from django.http import HttpResponse,HttpResponseRedirect
 from django.http import JsonResponse
 from django.core import serializers
 import json
+import ujson
 from .models import employee
+import jwt
+
+
 
 def hello(request):
     text = """<h1>welcome to my app !</h1>"""
@@ -38,3 +42,27 @@ def getApi(request):
     else:
         return JsonResponse({'success':False,'message':'Invalid request'})
         
+        
+def emplyLogin(request):
+    if request.method =="POST":
+        data = json.loads(request.body)
+        first_name = data["fname"]
+        last_name = data["lname"]   
+        getData = employee.objects.filter(first_name=first_name,last_name=last_name)
+        tmpJson = serializers.serialize("json",getData)
+        resp_json = json.loads(tmpJson)
+        
+        for d in resp_json:
+            del d['pk']
+            del d['model']
+            
+        if len(resp_json) ==0:
+            return JsonResponse({'success':False,'message':'Invalid credentials'})
+        else:
+            encoded_jwt = jwt.encode({'first_name':resp_json[0]["fields"]["first_name"] }, 'secret12345', algorithm='HS256')
+            # decoded_jwt = jwt.decode(encoded_jwt, 'secret12345', algorithms=['HS256'])
+            # print(decoded_jwt)
+            return JsonResponse({'success':True,'message':'success','token':ujson.loads(ujson.dumps(encoded_jwt))})
+
+    else:
+        return JsonResponse({'success':False,'message':'Invalid request'})
